e560b655a13350b61185ca2d1a3ddf9901c3aa32
Matias Cantoni <matiascantoni@hotmail.com>
2023-05-11 17:14:53 -0300

    creo carousel en el main

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const rectangle9 = {
+    width: "250px",
+    height: "304px",
+    padding: "8px 8px 8px 8px",
+    background: "#f6f4ee",
+    borderColor: "#000000",
+    borderWidth: "1px",
+    borderStyle: "solid",}
